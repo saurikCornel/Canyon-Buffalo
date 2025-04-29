49f47ccc7f5ef7727a145382c97adcd6274e4648
Alex Belov <alex@cornel.su>
2025-04-29 16:07:19 +0300

    Initial Commit

---
+++ Canyon Buffalo Games/Canyon_Buffalo_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canyon_Buffalo_GamesApp.swift
+//  Canyon Buffalo Games
+//
+//  Created by alex on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Canyon_Buffalo_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
